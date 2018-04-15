287b3e56f3b51c2f4dd7bdbd2ca021b1281ffef5
securekim <admin@securekim.com>
2018-04-16 00:55:02 +0900

    [Feature] Add list, Add Delete button in Private

--- main/js/main.js
+++ main/js/main.js
@@ -1,68 +1,11 @@
 
-/*
-var oTbl;
-//Row 추가
-function insRow() {
-  oTbl = document.getElementById("addTable");
-  var oRow = oTbl.insertRow();
-  oRow.onmouseover=function(){oTbl.clickedRowIndex=this.rowIndex}; //clickedRowIndex - 클릭한 Row의 위치를 확인;
-  var oCell = oRow.insertCell();
-
-
-//                                      <table id="addTable" width="350px" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" border="0">
-//                                        <tr>
-//                                          <td>
-//                                            <input type="text" name="addText" style="width:300px; height:30px;">
-//                                          </td>
-//                                          <td rowspan="2">
-//                                              <button class="btn btn-outline-info my-2 my-sm-0" type=button value="추가" onClick="insRow()" style="cursor:hand; height:50px;">+</button>
-//                                          </td>
-//                                        </tr>
-//                                        <tr style="padding-bottom:10px;">
-//                                          <td style="padding-top:10px;">
-//                                            <input type="text" name="addText" style="width:300px; height:30px;">
-//                                            </td>
-//                                        </tr>
-//                                      </table>
-
-
-
-  //삽입될 Form Tag
-  var frmTag = '<input type=text name=addText style="width:300px; height:30px;"> ';
-  frmTag += '<button class="btn btn-outline-danger my-2 my-sm-0" type=button value="삭제" onClick="removeRow()" style="cursor:hand; height:50px;">X</button>';
-  frmTag += '<input type=text name=addText style="width:300px; height:30px;">'
-  oCell.innerHTML = frmTag;
-}
-//Row 삭제
-function removeRow() {
-  oTbl.deleteRow(oTbl.clickedRowIndex);
-}
-
-function frmCheck()
-{
-  var frm = document.form;
-  
-  for( var i = 0; i <= frm.elements.length - 1; i++ ){
-     if( frm.elements[i].name == "addText" )
-     {
-         if( !frm.elements[i].value ){
-             alert("Input Text Box!");
-                 frm.elements[i].focus();
-	 return;
-          }
-      }
-   }
- }
- */
-
 function insTable(){
   var tables = document.getElementById('tables');
   
   var table = document.createElement('table');  
+    table.className = "contents_table";
     table.width = "350px";
     table.style = "margin: 0px auto; padding-top:10px"
-    table.cellspacing="0";
-    table.cellpadding="0";
     table.bgcolor="#FFFFFF";
     table.border="0";
     tables.appendChild(table);
@@ -83,7 +26,7 @@ function insTable(){
     td2.rowSpan="2";
     tr1.appendChild(td2);  
 
-  var frmTag2 = '<button class="btn btn-outline-danger my-2 my-sm-0" type=button value="삭제" onClick="removeRow()" style="cursor:hand; height:50px;">χ</button>';
+  var frmTag2 = '<button class="btn btn-outline-danger my-2 my-sm-0" type=button value="삭제" onClick="removeTable($(this))" style="cursor:hand; height:50px;">X</button>';
     td2.innerHTML=frmTag2;  
 
   var tr2 = document.createElement('tr');
@@ -99,6 +42,6 @@ function insTable(){
 
 }
 
-function removeRow() {
-  oTbl.deleteRow(oTbl.clickedRowIndex);
+function removeTable(td) {
+  console.log(td.parent().parent().parent().parent().remove());
 }
